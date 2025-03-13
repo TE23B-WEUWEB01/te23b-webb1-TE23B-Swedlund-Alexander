3c1e6562f33880d6c2dbd2e610b0a19e12f82a90
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-03-13 11:08:22 +0100

    commit

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,10 @@
+var ålder = 17;
+var tal1 =12;
+var tal2 =24;
+var summa =tal1+tal2;
+var namn ="awf";
+var nman = "faw";
+var namnman = namn +" "+nman;
+var hob="aga";
+var men =namnman + "'s"+" " + "afa"+" "+hob;
+alert(men); 
