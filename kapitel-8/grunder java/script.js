8ca78060ffb38b7d38a37c5fa3694436c0960f36
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-03-14 14:04:16 +0100

    commit

---
+++ kapitel-8/grunder java/script.js
@@ -0,0 +1,31 @@
+//var dag = "fredag";
+//alert(dag);
+//console.log("idag är det "+dag);
+//var vart = prompt("Var är du född");
+//console.log("du är frn "+vart);
+//var giss = prompt("är då inte inte en människa");
+//if (giss == "ja"){
+  //  alert("you be homan");
+//} else {
+ //   alert("you no hooman");
+//}
+//var slp1 = Math.random();
+//document.writeln(slp1);
+//var slp1 =Math.round(Math.random()* 100);
+// var slp2 =Math.round(Math.random()* 100); 
+// var gissning = prompt ("vad är "+ slp1 + "+" + slp2 + "?");
+// if (condition) {
+//     document.writeln("<h1> de e människa!</h1>");
+// } else { 
+//     document.writeln("<h1> de e en äklig robot!</h1>");
+
+// }
+// var tal1 =prompt ("number one");
+// var tal2 =prompt ("number two");
+// var sum = Math(tal1 + tal2);
+// document.writeln (tal1 + " + " + tal2 + " = " + sum);
+var inkom = prompt ("how much u ern");
+var skat = prompt ("what percent tax");
+var skt2 = Number(skat/100);
+var skarr = Number(inkom) *Number(skt2);
+alert(skarr);
